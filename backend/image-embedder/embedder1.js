9fa62de0426155ec986857f11906053a4c091447
rafi-d-z <rafidizia@gmail.com>
2024-05-02 03:32:03 -0400

    tried using image-bind, getting api issues

---
+++ backend/image-embedder/embedder1.js
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv'
+import Replicate from 'replicate';
+import fs from 'fs'
+
+dotenv.config()
+const apiKey = process.env.REPLICATE_API_TOKEN
+const replicate = new Replicate({
+    auth: apiKey,
+})
+let image;
+
+fs.readFile('../image-embedder/superman.jpg', (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    image = data
+})
+
+/* const input = {
+    'input': 'guacamole sauce',
+    'modality': "text"
+}; */
+
+const embedding = await replicate.run(
+    "daanelson/imagebind:0383f62e173dc821ec52663ed22a076d9c970549c209666ac3db181618b7a304", {
+        input: {
+            'input': 'guacamole sauce',
+            'modality': "text"
+        }
+    }
+);
+
+console.log(embedding)
+
+
+
