e2079933089646a1fe4292fc91ebd175a81ee700
darcierre <darrnurse@gmail.com>
2024-05-05 12:40:43 -0400

    Port update for heroku.

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const storageEngine = multer.diskStorage({
   destination: (request, file, callback) => {
